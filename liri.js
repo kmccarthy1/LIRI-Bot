0751f266f3208cccb7d4fc184d0eb6bf23d230e3
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-10-15 14:49:36 -0400

    added vars to liri.js

---
+++ liri.js
@@ -0,0 +1,22 @@
+
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+
+
+//module to write output to console & log.txt
+var log = require('simple-node-logger').createSimpleFileLogger(filename);
+log.setLevel('all');
+
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += '+' + process.argv[i];
+}
